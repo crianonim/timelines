f9105d4abb2ae05f85b6b168f6f71d05efeb6b61
Jan Skowronski <crianonim@gmail.com>
2023-04-29 12:19:52 +0100

    add note saving

--- src/Notes/ListNotes.elm
+++ src/Notes/ListNotes.elm
@@ -1,25 +1,30 @@
 module Notes.ListNotes exposing (..)
 
 import Html exposing (Attribute, Html, a, button, div, h1, text)
-import Html.Attributes exposing (href)
-import Html.Events exposing (onClick)
+import Html.Attributes as Attrs exposing (href)
+import Html.Events as Events exposing (onClick)
 import Http
 import Notes.Note as Note exposing (Note)
 
 
 type alias Model =
     { notes : List Note
+    , content : String
     }
 
 
 type Msg
     = GetNotes
     | GotNotes (Result Http.Error (List Note))
+    | ContentInput String
+    | CreateNote
+    | NoteCreated (Result Http.Error Note)
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { notes = []
+      , content = ""
       }
     , Note.getNotes GotNotes
     )
@@ -33,6 +38,12 @@ update msg model =
 
         GotNotes (Ok notes) ->
             ( { model | notes = notes }, Cmd.none )
+
+        ContentInput str ->
+            ( { model | content = str }, Cmd.none )
+
+        CreateNote ->
+            ( model, Note.createNote model.content NoteCreated )
 
         _ ->
             ( model, Cmd.none )
@@ -50,9 +61,15 @@ view model =
                 model.notes
             )
         , button [ onClick GetNotes ] [ text "Get Notes" ]
+        , Html.input [ Attrs.value model.content, Events.onInput ContentInput ] []
+        , Html.button [ Events.onClick CreateNote ] [ Html.text "Create Note" ]
         ]
 
 
 viewNote : Note -> Html Msg
 viewNote n =
-    div [] [ text (String.fromInt n.id), text " - ", div [] [ a [ href <| "notes/"  ++ String.fromInt n.id ] [ text n.content ] ] ]
+    div []
+        [ text (String.fromInt n.id)
+        , text " - "
+        , div [] [ a [ href <| "notes/" ++ String.fromInt n.id ] [ text n.content ] ]
+        ]
